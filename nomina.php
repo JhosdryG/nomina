ab35aaffb0e5ee4f4b2395a6555e6814e9b58e60
Ale <alejandrogonzalezduarte@hotmail.com>
2020-07-27 17:36:31 -0400

    añado nomina

---
+++ nomina.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/nomina.css">
+</head>
+<body>
+    <table> 
+        <tr>
+          <th>N°</th>
+          <th>APELLIDOS Y NOMBRES</th>
+          <th>IDENTIFICACIÓN</th>
+          <th>FECHA DE INGRESO</th>
+          <th>COSTO HORA</th>
+          <th>NÚMERO DE HORAS</th>
+          <th>SUELDO BASE</th>
+          <th>% POR ANTIGÜEDAD</th>
+          <th>TOTAL POR ANTIGÜEDAD</th>
+          <th>PRIMA POST GRADO</th>
+          <th>PRIMA POR HIJOS</th>
+          <th>TOTAL ASIGNACIONES</th>
+          <th>SUELDO INTEGRAL</th>
+          <th>IVSS</th>
+          <th>DEDUCIÓN</th>
+          <th>TOTAL RETENCIÓN Y DEDUCCIÓN</th>
+          <th>NETO A RECIBIR</th>
+          <th>1 ERA QUINCENA</th>
+          <th>2 DA QUINCENA</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Acosta Manzanilla, Javier Jose</td>
+          <td>V-14.545.934</td>
+          <td>22/10/2018</td>
+          <td>4368,27</td>
+          <td>24</td>
+          <td>419.353,92</td>
+          <td>25%</td>
+          <td>104.838,48</td>
+          <td>0,00</td>
+          <td>0,00</td>
+          <td>188.709,26</td>
+          <td>608.063,18</td>
+          <td>24.322,53</td>
+          <td>0,00</td>
+          <td>33.443,48</td>
+          <td>784.296,67</td>
+          <td>392.148,33</td>
+          <td>392.148,33</td>
+        </tr>
+      </table>
+</body>
+</html>
